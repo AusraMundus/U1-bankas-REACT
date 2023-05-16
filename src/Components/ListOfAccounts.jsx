f026a8efc3c553a1d0c330be1f85543cdebe9af7
Ausra <munduswebstudio@gmail.com>
2023-05-16 23:17:02 +0300

    Layout design

--- src/Components/ListOfAccounts.jsx
+++ src/Components/ListOfAccounts.jsx
@@ -6,23 +6,27 @@ export default function ListOfAccounts() {
     return (
         <>
             <h2>Sąskaitų sąrašas</h2>
-            <div>
-                <div className="accountsList">
-                    <p>Vardas</p>
-                    <p>Pavardė</p>
-                    <p>Balansas</p>
-                    <form>
-                        <fieldset className="fieldset">
-                            <input type="number" className="fieldset-input"/>
-                            <div className="fieldset-buttons">
-                                <button className="button">Pridėti lėšų</button>
-                                <button className="button">Nuskaičiuoti lėšas</button>
-                            </div>
-                        </fieldset>
-                    </form>
-                    <button className="button-del">Ištrinti sąskaitą</button>
-                </div>
-            </div>
+            <ul className="no-bullets">
+                <li>
+                    <div>
+                        <div className="accountsList">
+                            <p>Vardas</p>
+                            <p>Pavardė</p>
+                            <p>Balansas</p>
+                            <form>
+                                <fieldset className="fieldset">
+                                    <input type="number" className="fieldset-input" />
+                                    <div className="fieldset-buttons">
+                                        <button className="button">Pridėti lėšų</button>
+                                        <button className="button">Nuskaičiuoti lėšas</button>
+                                    </div>
+                                </fieldset>
+                            </form>
+                            <button className="button-del">Ištrinti sąskaitą</button>
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </>
     )
 }
